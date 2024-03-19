aa8598bf3034d4d880b68d3a445723d2a7cc63c8
Adepeju <125843413+adepejutee@users.noreply.github.com>
2024-03-19 01:27:29 -0500

    Add files via upload

---
+++ qrcoder.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 01:24:07 2024
+
+@author: ADEPEJU
+"""
+
+import qrcode
+qr = qrcode.QRCode(version=1, box_size=10, border=5)
+qr.add_data("https://www.facebook.com")
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("facebook.png")
